beb37b7fc379894dfbfe2233b91bec873f3da31c
daliaawad <awaddalia60@gmail.com>
2019-12-01 15:00:57 +0200

    control panel CSS

--- admin/src/Nav.tsx
+++ admin/src/Nav.tsx
@@ -5,16 +5,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
+      '& > *': {
+        margin: theme.spacing(1),
+      },
     },
   }));
 
@@ -22,105 +21,98 @@ const Nav: React.FC = () => {
     const classes = useStyles();
 
   return (
-      <div>
-    <div className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar className='Nav'>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <img id ='logo' src={serviceWorker}/>
-          <Typography id='title'>
-            Control Panel
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
-        <h2>DALIA</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
-        <h2>HELLOOO</h2>
+    <div id='main'>
+      <div className={classes.root}>
+        <AppBar position="fixed">
+          <Toolbar className='Nav'>
+            <img id ='logo' src={serviceWorker}/>
+            <Typography id='title'>
+              Control Panel
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <div>
+        <div id="side">
+          <br></br>
+          <strong><a href='#'>Profiles</a></strong>
+          <br></br>
+          <br></br>
+          <strong><a href='#'>Users</a></strong>
+        </div>
+      </div>
+      <div id='content'>
+        <div id='buttons'>
+          <Button variant="contained" className='tabs'>New</Button>
+          <Button variant="contained" className='tabs'>Accepted</Button>
+          <Button variant="contained" className='tabs'>Under Checking</Button>
+        </div>
+        <div id='profiles'>
+          <table >
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Show profile</th>
+            </tr>
+            <tr>
+              <td>1.</td>
+              <td>Maria Anders</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>2.</td>
+              <td>Christina Berglund</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>3.</td>
+              <td>Francisco Chang</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>4.</td>
+              <td>Roland Mendel</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>5.</td>
+              <td>Helen Bennett</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>6.</td>
+              <td>Philip Cramer</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>7.</td>
+              <td>Yoshi Tannamuri</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>8.</td>
+              <td>Giovanni Rovelli</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>9.</td>
+              <td>Simon Crowther</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+            <tr>
+              <td>10.</td>
+              <td>Marie Bertrand</td>
+              <Button variant="contained" className='tabs'>Show</Button>
+            </tr>
+          </table>
+        </div>
+      </div>
+        </div>
+        
     </div>
   );
 }
 
 export default Nav;
+
+
