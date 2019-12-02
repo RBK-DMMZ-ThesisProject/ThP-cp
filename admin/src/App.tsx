34f9ae37deee63dc21157facfba36e1b8cac0e07
maysqunaibi <maysqunaibi2000@gmail.com>
2019-12-02 17:10:34 +0200

    designing the front-end of signIn page

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import Nav from './Nav';
 import './App.css';
 import Profile from './Profile';
-
+import SignIn from './SignIn';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Nav/>
+      {/* <Nav/> */}
       {/* <Profile /> */}
+      <SignIn/>
     </div>
   );
 }
