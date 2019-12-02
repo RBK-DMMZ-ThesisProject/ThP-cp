a3b5989e4e594247b7f6e1bd9b6d62392057ebdf
daliaawad <awaddalia60@gmail.com>
2019-12-02 12:00:31 +0200

    adding tabs

--- admin/src/Nav.tsx
+++ admin/src/Nav.tsx
@@ -8,7 +8,40 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+
+function TabPanel(props : any) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.any.isRequired,
+//   value: PropTypes.any.isRequired,
+// };
+
+function a11yProps(index : any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 
 
@@ -16,9 +49,9 @@ const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
       maxWidth: 360,
-      backgroundColor: '#91cde0',
+      backgroundColor: '#c0e2ed',
       fontSize: '30px',
-      color: 'white',
+      color: '#63b8d4',
       fontWeight: 'bold',
       '& > *': {
         margin: theme.spacing(1),
@@ -34,11 +67,16 @@ function ListItemLink(props : any) {
 
 const Nav: React.FC = () => {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event : any, newValue : any) => {
+      setValue(newValue);
+    };
 
   return (
     <div id='main'>
       <div className={classes.root}>
-        <AppBar position="fixed">
+        <AppBar position="static">
           <Toolbar className='Nav'>
             <img id ='logo' src={serviceWorker}/>
             <Typography id='title'>
@@ -46,89 +84,157 @@ const Nav: React.FC = () => {
             </Typography>
           </Toolbar>
         </AppBar>
-        <Toolbar/>
-        <div className={classes.root}>
-          <List component="nav" aria-label="main mailbox folders" className="listText">
-            <ListItem button>
-              <ListItemText primary="Profiles"/>
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Users"/>
-            </ListItem>
-          </List>
+        </div>
+        <div id="main">
+          <div className={classes.root} id="list">
+            <List component="nav" aria-label="main mailbox folders" className="listText">
+              <ListItem button className="listItem">
+                <h5>Profiles</h5>
+              </ListItem>
+              <ListItem button className="listItem">
+                <h5>Users</h5>
+              </ListItem>
+            </List>
+        </div>
+        <div id='content'>
+          <AppBar position="static" id="tabs">
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" className="Tab">
+              <Tab label="Item One" {...a11yProps(0)} />
+              <Tab label="Item Two" {...a11yProps(1)} />
+              <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Maria Anders</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Christina Berglund</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>4.</td>
+                  <td>Roland Mendel</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>5.</td>
+                  <td>Helen Bennett</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td> 
+                </tr>
+                <tr>
+                  <td>6.</td>
+                  <td>Philip Cramer</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>7.</td>
+                  <td>Yoshi Tannamuri</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>8.</td>
+                  <td>Giovanni Rovelli</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>9.</td>
+                  <td>Simon Crowther</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td> 
+                </tr>
+                <tr>
+                  <td>10.</td>
+                  <td>Marie Bertrand</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+          <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Simon Crowther</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Giovanni Rovelli</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>4.</td>
+                  <td>Roland Mendel</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>5.</td>
+                  <td>Helen Bennett</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td> 
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+          <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Roland Mendel</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button variant="contained" className='tabs'>Show</Button></td>
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+        </div>
       </div>
-      <div id='content'>
-        <div id='buttons'>
-          <Button variant="contained" className='tabs'>New</Button>
-          <Button variant="contained" className='tabs'>Accepted</Button>
-          <Button variant="contained" className='tabs'>Under Checking</Button>
-        </div>
-        <div id='profiles'>
-          <table >
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>Name</th>
-                <th>Show profile</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1.</td>
-                <td>Maria Anders</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-              </tr>
-              <tr>
-                <td>2.</td>
-                <td>Christina Berglund</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-              </tr>
-              <tr>
-                <td>3.</td>
-                <td>Francisco Chang</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-              </tr>
-              <tr>
-                <td>4.</td>
-                <td>Roland Mendel</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-                </tr>
-              <tr>
-                <td>5.</td>
-                <td>Helen Bennett</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td> 
-                </tr>
-              <tr>
-                <td>6.</td>
-                <td>Philip Cramer</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-                </tr>
-              <tr>
-                <td>7.</td>
-                <td>Yoshi Tannamuri</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-                </tr>
-              <tr>
-                <td>8.</td>
-                <td>Giovanni Rovelli</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-                </tr>
-              <tr>
-                <td>9.</td>
-                <td>Simon Crowther</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td> 
-                </tr>
-              <tr>
-                <td>10.</td>
-                <td>Marie Bertrand</td>
-                <td><Button variant="contained" className='tabs'>Show</Button></td>
-                </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-        </div>
-        
     </div>
   );
 }
