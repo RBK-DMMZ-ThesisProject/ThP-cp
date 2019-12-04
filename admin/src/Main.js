4f46ab4d94861cc3689dcdba43563a91241c77d2
daliaawad <awaddalia60@gmail.com>
2019-12-04 19:05:52 +0200

    branches

---
+++ admin/src/Main.js
@@ -0,0 +1,390 @@
+import React from 'react';
+import './Nav.css';
+import $ from 'jquery';
+import serviceWorker from './serviceWorker.png';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Modal from '@material-ui/core/Modal';
+import Profile from './Profile';
+
+
+$.ajax({
+    type: "GET",
+    url: "/newprofils",  
+    datatype: JSON,
+    success:function(data){
+        console.log(data, 'NewProfiles')
+    },
+    error: function(request, status, error) {
+        console.log(error);
+        }
+});
+
+$.ajax({
+  type: "GET",
+  url: "/profils",  
+  datatype: JSON,
+  success:function(data){
+      console.log(data, 'UnderChecked Profiles')
+  },
+  error: function(request, status, error) {
+      console.log(error);
+      }
+});
+
+
+
+function getModalStyle() {
+  const top = 20;
+  const left = 20;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.any.isRequired,
+//   value: PropTypes.any.isRequired,
+// };
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 360,
+      backgroundColor: '#c0e2ed',
+      fontSize: '30px',
+      color: '#63b8d4',
+      fontWeight: 'bold',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    paper: {
+      position: 'absolute',
+      width: 1800,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+
+
+
+const Main = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+  return (
+    <div id='main'>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar className='Nav'>
+            <img id ='logo' src={serviceWorker} alt=""/>
+            <Typography id='title'>
+              Control Panel
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        </div>
+        <div id="main">
+          <div className={classes.root} id="list">
+            <List component="nav" aria-label="main mailbox folders" className="listText">
+              <ListItem button className="listItem">
+                <h5>Profiles</h5>
+              </ListItem>
+              <ListItem button className="listItem">
+                <h5>Users</h5>
+              </ListItem>
+            </List>
+        </div>
+        <div id='content'>
+          <AppBar position="static" id="tabs">
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" className="Tab">
+              <Tab label="New" {...a11yProps(0)} />
+              <Tab label="Accepted" {...a11yProps(1)} />
+              <Tab label="Under Checking" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Maria Anders</td>
+                  <td>
+                    <Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+          <TabPanel value={value} index={1}>
+          <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Simon Crowther</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Giovanni Rovelli</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>4.</td>
+                  <td>Roland Mendel</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>5.</td>
+                  <td>Helen Bennett</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td> 
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+          <table id="profiles">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Show profile</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Roland Mendel</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Francisco Chang</td>
+                  <td><Button type="button" onClick={handleOpen} id="modalBtn">
+                      Show
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Profile />
+                        </div>
+                      </Modal></td>
+                </tr>
+              </tbody>
+            </table>
+          </TabPanel>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
+//
+
+
+
---
+++ admin/src/Profile.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import './Profile.css';
+import serviceWorker from './serviceWorker.png';
+import { makeStyles } from '@material-ui/core/styles';
+import WorkImg from './WorkImg.jpg';
+import Button from '@material-ui/core/Button';
+import Revise from './Revise';
+import Modal from '@material-ui/core/Modal';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 360,
+    backgroundColor: '#c0e2ed',
+    fontSize: '30px',
+    color: '#63b8d4',
+    fontWeight: 'bold',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    position: 'absolute',
+    width: 1800,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+function getModalStyle() {
+  const top = 20;
+  const left = 20;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
+const Profile: React.FC = () => {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div id='main'>
+        <div>
+          <h1 id='name'>Zinah Al Nabahin</h1>
+          <hr></hr>
+          <div id="information">
+            <table>
+              <tbody>
+                <tr>
+                  <td className="Keys">Title:</td>
+                  <td className="values">Professional Hard Working Carpenter</td>
+
+                </tr>
+                <tr>
+                  <td className="Keys">Category:</td>
+                  <td className="values">Carpentering</td>
+                </tr>
+                <tr>
+                  <td className="Keys">Age:</td>
+                  <td className="values">32 years</td>
+                </tr>
+                <tr>
+                  <td className="Keys">Mobile No:</td>
+                  <td className="values">16549999</td>
+                </tr>
+                <tr>
+                  <td className="Keys">Email: </td>
+                  <td className="values">Zinah@hotmail.com</td>
+                </tr>
+                <tr>
+                  <td className="Keys">Description:</td>
+                  <td className="values">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</td>
+                </tr>
+                <tr>
+                  <td className="Keys">Image:</td>
+                  <td><img id ='serviceWorkerImg' src={serviceWorker} alt=""/></td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="vl"></div>
+            <div id="Sample">
+              <h2>Work Samples:</h2>
+              <img id ='workImg' src={WorkImg} alt=""/>
+              <div id="footerButtons">
+                <Button variant="contained" id="acceptBtn">Accept</Button><span></span>
+                <Button type="button" variant="contained" onClick={handleOpen} id="modalBtn">
+                      Revise
+                    </Button>
+                    <Modal
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      >
+                        <div style={modalStyle} className={classes.paper}>
+                          <Revise />
+                        </div>
+                      </Modal>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Profile;
