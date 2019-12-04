f3715370653ff3678c00490c505f2a7dd46e5dc3
daliaawad <awaddalia60@gmail.com>
2019-12-04 15:50:29 +0200

    adding the server

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Main from './Main.js';
 import './App.css';
-import Profile from './Profile';
 import SignIn from './SignIn';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Main />
       {/* <Nav/> */}
       {/* <Profile /> */}
-      <SignIn/>
+      {/* <SignIn/> */}
     </div>
   );
 }
