2f7019ff3fb5fc0df7af441045976069af226cdb
maysqunaibi <maysqunaibi2000@gmail.com>
2019-12-05 10:35:52 +0200

    connec signin with admin panel

--- admin/src/SignIn.js
+++ admin/src/SignIn.js
@@ -11,12 +11,15 @@ import {
 import $ from "jquery";
 import jwt_decode from "jwt-decode";
 import serviceWorker from "./serviceWorker.png";
+import Main from "./Main.js";
 //const bcrypt = require("bcryptjs");
 class LoginForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      password: ""
+      password: "",
+      showMain: false,
+      showLogin: true
     };
   }
   loginFun() {
@@ -49,57 +52,66 @@ class LoginForm extends React.Component {
     const decoded = jwt_decode(token);
     console.log(decoded.email);
     if (decoded.password === this.state.password) {
-      window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl", "_self");
+      //window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl", "_self");
+      this.setState({
+        showMain: true,
+        showLogin: false
+      });
     } else {
       alert("Wrong password or email");
     }
   }
-  render() {
-    return (
-      <Grid
-        textAlign="center"
-        style={{ height: "100vh" }}
-        verticalAlign="middle"
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h1" color="blue" textAlign="center">
-            <Image src={serviceWorker} s /> Log-in to your account
-          </Header>
-          <Form size="large" onSubmit={this.loginFun.bind(this)}>
-            <Segment stacked>
-              <Form.Input
-                id="email"
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="E-mail address"
-              />
-              <Form.Input
-                fluid
-                id="password"
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                type="password"
-              />
 
-              <Button
-                type="submit"
-                color="blue"
-                fluid
-                size="large"
-                onClick={this.getUser.bind(this)}
-              >
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
-            contact with other Admins <a href="#">here</a>
-          </Message>
-        </Grid.Column>
-      </Grid>
-    );
+  render() {
+    if (this.state.showLogin) {
+      return (
+        <Grid
+          textAlign="center"
+          style={{ height: "100vh" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h1" color="blue" textAlign="center">
+              <Image src={serviceWorker} /> Log-in to your account
+            </Header>
+            <Form size="large" onSubmit={this.loginFun.bind(this)}>
+              <Segment stacked>
+                <Form.Input
+                  id="email"
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
+                />
+                <Form.Input
+                  fluid
+                  id="password"
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                />
+
+                <Button
+                  type="submit"
+                  color="blue"
+                  fluid
+                  size="large"
+                  onClick={this.getUser.bind(this)}
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              contact with other Admins <a href="#">here</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      );
+    } else {
+      return <Main />;
+    }
   }
 }
 export default LoginForm;
