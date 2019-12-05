a355331ed84dcbebf4cc11082012afe9ffd55961
maysqunaibi <maysqunaibi2000@gmail.com>
2019-12-05 15:56:23 +0200

    adding routes for Home and admin pages

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import Main from './Main.js';
 import './App.css';
 import SignIn from './SignIn';
+import HomepageLayout from './Services';
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
 const App: React.FC = () => {
   return (
+    <Router>
     <div className="App">
       {/* <Main /> */}
       {/* <Nav/> */}
       {/* <Profile /> */}
-      <SignIn/>
+      <Switch> 
+          <Route path="/" exact component={HomepageLayout}/>
+      <Route path="/admin" component={SignIn}/>
+      
+      </Switch>
+    
     </div>
+    </Router>
   );
 };
 
