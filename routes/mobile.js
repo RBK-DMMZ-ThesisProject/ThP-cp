b003ca0d655994bf2fd9fd55a51a21663df69a11
znha <zihna-19@hotmail.com>
2019-12-06 14:43:25 +0200

    sending data to server from mobile t20

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,11 +3,10 @@ const mRouter = express.Router();
 const db = require("../database/db");
 const cors = require("cors");
 mRouter.use(cors());
-//Api that adds a new profile the  ServiceProvider table
+//Api that adds a new przzofile the  ServiceProvider table
 mRouter.post("/addNewProfile", (req, res) => {
-    var data = { "avatarSource": "avatar", "birthdate": "2019-12-06T11:46:43.195Z", "category": "Carpenter", "email": "Zihna-19@hotmail.com ", "familyName": "Dggghh", "firstName": "Dfgg", "phoneNum": "2345", "sampleWorkImg": "sw", "serverDesription": "Dhbbnn" }
-    db.saveNewProfile(data);
-    res.status(200).send('hello');
+    db.saveNewProfile(req.body);
+    res.status(200).json({ msg: 'saved' });
 
     //  db.saveNewProfile(req.body);
     // res.json(req.body);
