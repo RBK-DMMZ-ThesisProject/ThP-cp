a2c619bf79a78e8b2b25f777e72f6e05e52572a0
daliaawad <awaddalia60@gmail.com>
2019-12-07 14:23:52 +0200

    deploying

--- admin/src/SignIn.js
+++ admin/src/SignIn.js
@@ -67,55 +67,54 @@ class LoginForm extends React.Component {
   }
 
   render() {
-    if (this.state.showLogin) {
-      return (
-        <Grid
-          textAlign="center"
-          style={{ height: "100vh" }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h1" color="blue" textAlign="center">
-              <Image src={serviceWorker} /> Log-in to your account
-            </Header>
-            <Form size="large" onSubmit={this.loginFun.bind(this)}>
-              <Segment stacked>
-                <Form.Input
-                  id="email"
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="E-mail address"
-                />
-                <Form.Input
-                  fluid
-                  id="password"
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Password"
-                  type="password"
-                />
+    
+       var comp= this.state.showLogin? (<Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h1" color="blue" textAlign="center">
+            <Image src={serviceWorker} /> Log-in to your account
+          </Header>
+          <Form size="large" onSubmit={this.loginFun.bind(this)}>
+            <Segment stacked>
+              <Form.Input
+                id="email"
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="E-mail address"
+              />
+              <Form.Input
+                fluid
+                id="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              />
 
-                <Button
-                  type="submit"
-                  color="blue"
-                  fluid
-                  size="large"
-                  onClick={this.getUser.bind(this)}
-                >
-                  Login
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              contact with other Admins <a href="#">here</a>
-            </Message>
-          </Grid.Column>
-        </Grid>
-      );
-    } else {
-      return <Main />;
-    }
+              <Button
+                type="submit"
+                color="blue"
+                fluid
+                size="large"
+                onClick={this.getUser.bind(this)}
+              >
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            contact with other Admins <a href="#">here</a>
+          </Message>
+        </Grid.Column>
+      </Grid> ) : (<Main />)
+      return comp;
+  
+      
+
   }
 }
 export default LoginForm;
