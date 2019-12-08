028d19fe1648229662783bdc88436dc3a69d7669
mahanbahin <maha.a.nbahin@hotmail.com>
2019-12-08 20:11:58 +0200

    sending sms msges from the server side

---
+++ routes/sms.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const smsRouter = express.Router();
+const cors = require("cors");
+const client = require("twilio")(
+  process.env.TWILIO_ACCOUT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+smsRouter.use(cors());
+
+smsRouter.post("/smsMessages", (req, res) => {
+  console.log("heloooo");
+  res.header("Content-Type", "application/json");
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,,
+      to: req.body.to,
+      body: "Youre Profile was successfully add ^__^"
+    })
+    .then(() => {
+      console.log("succsee");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+module.exports = smsRouter;
--- server.js
+++ server.js
@@ -5,27 +5,29 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
-
+const pino = require("express-pino-logger")();
+console.log(process.env);
 // app.use(express.static(__dirname + "/"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.post('/test', (req, res) => {
-
-  res.status(200).send('hello');
-})
-app.use('/mobileApi', require("./routes/mobile.js"));
-app.use('/auth', require("./routes/auth.js"));
-
+app.post("/test", (req, res) => {
+  res.status(200).send("hello");
+});
+app.use(cors());
+app.use(pino);
+app.use("/mobileApi", require("./routes/mobile.js"));
+app.use("/auth", require("./routes/auth.js"));
+app.use("/smsApi", require("./routes/sms.js"));
 app.use(require("./routes"));
 
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('admin/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'admin', 'build', 'index.html'));
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("admin/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
