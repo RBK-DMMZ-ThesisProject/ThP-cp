f9d0e4c30e72005fb8e2181e211a9f1b980ac8b3
mahanbahin <maha.a.nbahin@hotmail.com>
2019-12-09 17:12:04 +0200

    api get catogrie's profiles

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,13 +3,17 @@ const mRouter = express.Router();
 const db = require("../database/db");
 const cors = require("cors");
 mRouter.use(cors());
-//Api that adds a new przzofile the  ServiceProvider table
+//Api that adds a new profile to the  ServiceProvider table
 mRouter.post("/addNewProfile", (req, res) => {
-    db.saveNewProfile(req.body);
-    res.status(200).json({ msg: 'saved' });
-
-    //  db.saveNewProfile(req.body);
-    // res.json(req.body);
+  db.saveNewProfile(req.body);
+  res.status(200).json({ msg: "saved" });
 });
-
+//Api that gets the useres for specific catogery
+mRouter.get("/getProfiles", (req, res) => {
+  db.ServiceProvider.find({ ServiceCategory: req.body.body })
+    .select("_id userName  userImg ")
+    .then(profils => {
+      res.send(profils);
+    });
+});
 module.exports = mRouter;
