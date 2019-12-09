4d6fb49dc22796abdf5648e537d5e483897ba52e
mahanbahin <maha.a.nbahin@hotmail.com>
2019-12-09 18:44:04 +0200

    api get catogrie's profiles

--- routes/sms.js
+++ routes/sms.js
@@ -2,8 +2,8 @@ const express = require("express");
 const smsRouter = express.Router();
 const cors = require("cors");
 const client = require("twilio")(
-  process.env.TWILIO_ACCOUT_SID,
-  process.env.TWILIO_AUTH_TOKEN
+  "AC084f99ac4b6c70a1f38ba0d5a6a46dad",
+  "6d56a0e4fc0f623e1089a24ae247bfb1"
 );
 
 smsRouter.use(cors());
@@ -13,7 +13,7 @@ smsRouter.post("/smsMessages", (req, res) => {
   res.header("Content-Type", "application/json");
   client.messages
     .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
+      from: "+19738162205",
       to: req.body.to,
       body: "Youre Profile was successfully add ^__^"
     })
