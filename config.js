3839efa1939f28b86cb432d82252a016532d7aed
znha <zihna-19@hotmail.com>
2019-12-15 14:51:42 +0200

    fixing deployemnt issues

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const STRIPE_SECRET = "sk_live_rbTsh1w33Yah1dzwCKLTVvto00To2919i4"
-export const JWT_SECRET = "secret"
+module.exports.STRIPE_SECRET = "sk_live_rbTsh1w33Yah1dzwCKLTVvto00To2919i4"
+module.exports.JWT_SECRET = "secret"
