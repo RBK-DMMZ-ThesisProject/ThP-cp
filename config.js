a28fed6dac83932bbd92fe75d8df12a01f52b1d9
znha <zihna-19@hotmail.com>
2019-12-17 20:24:28 +0200

    updating config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-module.exports.STRIPE_SECRET = "sk_live_K6bztAdT2dZpIY0JaQS8YBye002snSTfX9";
+module.exports.STRIPE_SECRET = "sk_test_OMrfdqPYt99p355xTnjc9zlm00k16BvwGn";
 module.exports.JWT_SECRET = "secret";
